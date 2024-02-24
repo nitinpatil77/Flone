43a8ec12d6d8ec541a94aee2712a68e74d04df3f
NitinPatil <patilnitin1437@gmail.com>
2024-02-24 15:16:23 +0530

    slider

---
+++ assets/js/slider.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    document.querySelector('.prev-btn').addEventListener('click', prevSlide);
+    document.querySelector('.next-btn').addEventListener('click', nextSlide);
+
+    showSlide(currentSlide);
+});
