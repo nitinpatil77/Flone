1feabaedfc041ecf0959f341f87169f819186576
Nitn Patil <131695309+nitinpatil77@users.noreply.github.com>
2024-03-07 12:59:32 +0000

    updated

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -7,6 +7,7 @@ window.onload = () => {
         document.getElementById("empty-item").style.display = "block";
         document.getElementById("full-fill-item").style.display = "none";
       } else {
+        checkout();
         document.getElementById("empty-item").style.display = "none";
         document.getElementsByClassName("cart")[0].innerHTML = productJSON.length;
       }
