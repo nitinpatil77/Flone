96584c64c9964222a47917e5701b847016e5875f
Nitn Patil <131695309+nitinpatil77@users.noreply.github.com>
2024-03-07 11:58:09 +0000

    checkout empty cart js functionality added

---
+++ assets/js/checkout.js
@@ -0,0 +1,14 @@
+var productJSON = [];
+window.onload = () => {
+    productJSON = JSON.parse(localStorage.getItem("cartItems")) || [];
+   
+    var len = productJSON.length;
+    if (len === 0) {
+        document.getElementById("empty-item").style.display = "block";
+        document.getElementById("full-fill-item").style.display = "none";
+      } else {
+        document.getElementById("full-fill-item").style.display = "none";
+        addtocartPage();
+        document.getElementsByClassName("cart")[0].innerHTML = productJSON.length;
+      }
+}
