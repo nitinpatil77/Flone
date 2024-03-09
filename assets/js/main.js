8957797d9cdbcf25ccbf996393361ba756941289
NitinPatil <patilnitin1437@gmail.com>
2024-03-09 23:05:44 +0530

    offcanvas js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,25 @@ document.addEventListener("DOMContentLoaded", function() {
         currentSlide = (currentSlide - 1 + slides.length) % slides.length;
         showSlide(currentSlide);
     }
-
     document.querySelector('.prev-btn').addEventListener('click', prevSlide);
     document.querySelector('.next-btn').addEventListener('click', nextSlide);
 
     showSlide(currentSlide);
+});
+
+// offcanvas 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mobileAsideButton = document.querySelector('.mobile-aside-button');
+    var offcanvasMobileMenu = document.querySelector('#offcanvas-mobile-menu');
+
+    mobileAsideButton.addEventListener('click', function() {
+        offcanvasMobileMenu.classList.add('active');
+    });
+
+    // Close the off-canvas menu when clicking on the close button
+    var mobileMenuCloseTrigger = document.querySelector('#mobile-menu-close-trigger');
+    mobileMenuCloseTrigger.addEventListener('click', function() {
+        offcanvasMobileMenu.classList.remove('active');
+    });
 });
