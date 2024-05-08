12860586fdf49907aa583152d77403399fdbb368
Nitn Patil <131695309+nitinpatil77@users.noreply.github.com>
2024-05-08 12:59:43 +0000

    modification done

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -16,18 +16,21 @@ window.onload = () => {
   }
 }
 checkout = () => {
+  let grandtotal=0;
   document.getElementById("your-order-middle").innerHTML = productJSON
     .map((item) => {
       let { id, name, price, discount, product_main_img, qty, subtotal } = item;
       const discountedPrice = price - price * (discount / 100);
 
       subtotal = discount ? discountedPrice : price;
-      subtotal = (subtotal * qty).toFixed(2);
+      subtotal = (subtotal * qty);
+      grandtotal += subtotal; 
+      document.getElementById("your-order-total").innerHTML=grandtotal;
       return `
               <ul>
                   <li class="d-flex justify-content-between">
                             <span class="order-middle-left">${name}</span>
-                            <span class="order-price">${discountedPrice}</span>
+                            <span class="order-price">${subtotal}</span>
                   </li>
               </ul>
                `;
@@ -35,22 +38,5 @@ checkout = () => {
     .join("");
 };
 
-var check = () => {
-  let grandtotal = 0; // Initialize grandtotal
-  
-  productJSON.forEach((item) => {
-    let { price, discount, qty } = item;
-    const discountedPrice = price - price * (discount / 100);
-    const subtotal = discount ? discountedPrice * qty : price * qty;
-    grandtotal += subtotal; // Accumulate subtotal to grandtotal
-  });
-
-  // Display grandtotal in the HTML
-  document.getElementById("your-order-total").innerHTML = `
-    <ul class="d-flex justify-content-between">
-      <li class="order-total">Total</li>
-      <li>${grandtotal.toFixed(2)}</li>
-    </ul>`;
-};
 
 
